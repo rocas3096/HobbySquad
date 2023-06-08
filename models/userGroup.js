1e8aa5e97fef5c0cf5d685a9ce5296d46dd31409
rocas3096 <ozavala3096@gmail.com>
2023-06-07 21:36:50 -0500

    Update userGroup.js

--- models/userGroup.js
+++ models/userGroup.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const sequelize = require("../config/connection");
 const User = require("./user");
 const Group = require("./group");
 
