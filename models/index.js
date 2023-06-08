32d7a733912e99d57ec15864bb15fb6e81b350c0
rocas3096 <ozavala3096@gmail.com>
2023-06-07 21:19:36 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,25 +1,25 @@
-const sequelize = require('../config/database');
-const User = require('./User');
-const Group = require('./Group');
-const Hobby = require('./Hobby');
-const UserGroup = require('./UserGroup');
-const UserHobby = require('./UserHobby');
+const sequelize = require("../config/database");
+const User = require("./user");
+const Group = require("./group");
+const Hobby = require("./hobby");
+const UserGroup = require("./userGroup");
+const UserHobby = require("./userHobby");
 
-User.hasMany(UserHobby, { foreignKey: 'user_id' });
-UserHobby.belongsTo(User, { foreignKey: 'user_id' });
+User.hasMany(UserHobby, { foreignKey: "user_id" });
+UserHobby.belongsTo(User, { foreignKey: "user_id" });
 
-Hobby.hasMany(UserHobby, { foreignKey: 'hobby_id' });
-UserHobby.belongsTo(Hobby, { foreignKey: 'hobby_id' });
+Hobby.hasMany(UserHobby, { foreignKey: "hobby_id" });
+UserHobby.belongsTo(Hobby, { foreignKey: "hobby_id" });
 
 User.belongsToMany(Group, {
   through: UserGroup,
-  foreignKey: 'user_id',
-  otherKey: 'group_id',
+  foreignKey: "user_id",
+  otherKey: "group_id",
 });
 Group.belongsToMany(User, {
   through: UserGroup,
-  foreignKey: 'group_id',
-  otherKey: 'user_id',
+  foreignKey: "group_id",
+  otherKey: "user_id",
 });
 
 sequelize.sync({ alter: true }); // Sync the models with the database
