4e28e3f0363f92bc468c38150ca8c2505d77d207
rocas3096 <ozavala3096@gmail.com>
2023-06-09 17:46:24 -0500

    Update index.js
    Added other routes to api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require("./user-routes");
+const groupRoutes = require("./group-routes");
+const hobbyRoutes = require("./hobby-routes");
+const tagRoutes = require("./tag-routes");
 
-router.use('/users', userRoutes)
+router.use('/users', userRoutes);
+router.use('/groups', groupRoutes);
+router.use('/hobbies', hobbyRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
