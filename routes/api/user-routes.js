40e410450560b487f0d6d7dca531c44865d92c11
rocas3096 <ozavala3096@gmail.com>
2023-06-09 17:48:15 -0500

    Update user-routes.js
    Updated route to allow for updates of multiple portions of user profile.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -40,7 +40,7 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-    const { Hobbies } = req.body;
+    const { username, password, Hobbies } = req.body;
 
     const updatedUser = await User.findByPk(req.params.id);
 
@@ -49,12 +49,17 @@ router.put("/:id", async (req, res) => {
       return;
     }
 
+    // Update the user's username and password
+    updatedUser.username = username;
+    updatedUser.password = password;
+    await updatedUser.save();
+
     // Update the user's hobbies
     await updatedUser.setHobbies(Hobbies);
 
     // Fetch the updated user with associated hobbies
     const userWithHobbies = await User.findByPk(req.params.id, {
-      include: [Hobby],
+      include: [Group, Hobby],
     });
 
     res.status(200).json(userWithHobbies);
