6adbf86838fdb262cbef16ffa23243bc11da4bb2
rocas3096 <ozavala3096@gmail.com>
2023-06-20 16:04:12 -0500

    Update leave-group.js

--- public/javascript/leave-group.js
+++ public/javascript/leave-group.js
@@ -3,16 +3,15 @@ const leaveGroupButton = document.querySelector(".leave-group");
 leaveGroupButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // Perform an AJAX request to the server to leave the group
+
   fetch(event.target.href, {
     method: "DELETE",
   })
     .then((response) => {
       if (response.ok) {
-        // Refresh the page or update the UI accordingly
+        // Refresh the page
         location.reload();
       } else {
-        // Handle the error response
         console.error("Failed to leave the group");
       }
     })
