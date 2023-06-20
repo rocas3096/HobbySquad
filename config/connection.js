cc29d717a04a84c642c4e37fbff33fce1af2a2a5
rocas3096 <ozavala3096@gmail.com>
2023-06-20 17:17:24 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
+// Update the database configuration to use Heroku's environment variables
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.JAWSDB_URL || process.env.DB_NAME,
+  process.env.JAWSDB_USERNAME || process.env.DB_USER,
+  process.env.JAWSDB_PASSWORD || process.env.DB_PASSWORD,
   {
-    host: "localhost",
-    dialect: "mysql",
+    host: process.env.JAWSDB_HOST || "localhost",
+    dialect: process.env.JAWSDB_DIALECT || "mysql",
     dialectOptions: {
       decimalNumbers: true,
     },
