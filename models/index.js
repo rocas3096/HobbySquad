acb729fabc6f6283b4266d41b69b0c5ac157c95b
rocas3096 <ozavala3096@gmail.com>
2023-11-16 09:35:55 -0600

    Update index.js in models folder
    Trying to clean up models to deploy correctly on heroku

--- models/index.js
+++ models/index.js
@@ -3,29 +3,37 @@ const Post = require("./post");
 const Tag = require("./tag");
 const User = require("./user");
 const UserGroup = require("./userGroup");
-// const Post = require("./post");
+
 // Associations
 
+// Group associations
 Group.belongsTo(User, {
   foreignKey: "owner_id",
-});
-Tag.belongsTo(Group, { foreignKey: "group_id" });
-User.belongsToMany(Group, {
-  through: UserGroup,
-  foreignKey: "UserId",
-});
-Group.belongsToMany(User, {
-  through: UserGroup,
-  foreignKey: "GroupId",
 });
 Group.hasOne(User, {
   foreignKey: "owner_id",
 });
-UserGroup.hasMany(User, { foreignKey: "UserId" });
-UserGroup.hasMany(Group, { foreignKey: "GroupId" });
+Tag.belongsTo(Group, { foreignKey: "group_id" });
 Group.hasMany(Tag, { foreignKey: "group_id" });
 Post.belongsTo(Group, { foreignKey: "group_id" });
+
+// User associations
+User.belongsToMany(Group, {
+  through: UserGroup,
+  foreignKey: "UserId",
+});
+UserGroup.belongsTo(User, { foreignKey: "UserId" });
+Group.belongsToMany(User, {
+  through: UserGroup,
+  foreignKey: "GroupId",
+});
+
+// UserGroup associations
+UserGroup.belongsTo(Group, { foreignKey: "GroupId" });
+
+// Post associations
 Post.belongsTo(User, { foreignKey: "user_id" });
+
 // Export the models
 module.exports = {
   User,
